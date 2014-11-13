4d47e265aa2d89862b2cf407b42a301758cc4c89
Graham McBain <grahammcbain@gmail.com>
2014-11-13 15:31:30 -0700

    added bookmarklet helpercode

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -31,6 +31,21 @@ Template[getTemplate('post_submit')].rendered = function(){
     callback(instance);
   });
 
+  var urlParams;
+  (window.onpopstate = function () {
+      var match,
+          pl     = /\+/g,  // Regex for replacing addition symbol with a space
+          search = /([^&=]+)=?([^&]*)/g,
+          decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+          query  = window.location.search.substring(1);
+
+      urlParams = {};
+      while (match = search.exec(query))
+         urlParams[decode(match[1])] = decode(match[2]);
+  })();
+  $('#url').val(urlParams["url"]);
+  $('#title').val(urlParams["title"]);
+}
   Session.set('currentPostStatus', STATUS_APPROVED);
   Session.set('selectedPostId', null);
   if(!this.editor && $('#editor').exists())
