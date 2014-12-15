cdb3e3d088344170e48a9c0d477c332ac1557186
Graham McBain <grahammcbain@gmail.com>
2014-12-15 14:31:23 -0700

    bookmarklet works

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -4,20 +4,7 @@ AutoForm.hooks({
       
       var properties = insertDoc;
       var submit = this;
-      var urlParams;
-        (window.onpopstate = function () {
-          var match,
-              pl     = /\+/g,  // Regex for replacing addition symbol with a space
-              search = /([^&=]+)=?([^&]*)/g,
-              decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-              query  = window.location.search.substring(1);
-
-          urlParams = {};
-          while (match = search.exec(query))
-               urlParams[decode(match[1])] = decode(match[2]);
-        })();
-        $('input[name=url]').val(urlParams["url"]);
-        $('input[name=title]').val(urlParams["title"]);
+      
 
       // ------------------------------ Checks ------------------------------ //
 
